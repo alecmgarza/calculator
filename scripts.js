d50f2e07fd81782cb34d5c5e3372acf6e4b4a50e
Alec Garza <alecmgarza@gmail.com>
2022-07-29 16:54:11 -0500

    Press number buttons to display numbers on calculator, change div names to specify between number buttons and operator buttons

--- scripts.js
+++ scripts.js
@@ -17,3 +17,16 @@ function divide(a, b) {
 function operate(operator, a, b) {
     return operator(a, b);
 };
+
+function display(input) {
+    const display = document.querySelector('#display');
+    display.textContent += input;
+}
+
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        display(number.textContent);
+    })
+})
