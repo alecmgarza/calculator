fe48a7e16b55d3a8c87c40141d9ed8d8003974f1
Alec Garza <alecmgarza@gmail.com>
2022-07-29 20:32:07 -0500

    Add functionality to operator and = buttons so calculator can solve problems with two numbers

--- scripts.js
+++ scripts.js
@@ -1,25 +1,32 @@
 function add(a, b) {
-    return a + b;
+    display.textContent = a + b;
 };
 
 function subtract(a, b) {
-    return a - b;
+    display.textContent = a - b;
 };
 
 function multiply(a, b) {
-    return a * b;
+    display.textContent = a * b;
 };
 
 function divide(a, b) {
-    return a / b;
+    display.textContent = a / b;
 };
 
 function operate(operator, a, b) {
-    return operator(a, b);
+    if (operator === 'add') {
+        add(a, b);
+    } else if (operator === 'subtract') {
+        subtract(a, b);
+    } else if (operator === 'multiply') {
+        multiply(a, b);
+    } else if (operator === 'divide') {
+        divide(a, b);
+    }
 };
 
 const display = document.querySelector('#display');
-let displayValue = display.textContent;
 
 function displayNumber(input) {
     display.textContent += ''; 
@@ -31,6 +38,25 @@ const numbers = document.querySelectorAll('.number');
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
         displayNumber(number.textContent);
+    })
+})
+
+const operations = document.querySelectorAll('.operations');
+
+operations.forEach((operation) => {
+    operation.addEventListener('click', () => {
+        let firstNumber = (+display.textContent);
+        console.log(firstNumber);
+        display.textContent = '';
+        let operator = operation.id;
+        console.log(operator);
+        const equals = document.querySelector('.calculate');
+        equals.addEventListener('click', () => {
+            let secondNumber = (+display.textContent);
+            console.log(secondNumber);
+            operate(operator, firstNumber, secondNumber);
+
+        })
     })
 })
 
